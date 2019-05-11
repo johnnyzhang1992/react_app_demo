7e512c31482225c77fb5ab65a9a3c37a4826ce6d
johnnyzhang1992 <zq19920306@gmail.com>
2019-05-11 19:37:10 +0800

    echarts for react demo

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import logo from './logo.svg';
 // import Todo from './todo/index'
 // import Email from './components/Email';
 // import Clock from './components/Clock';
-import ContextDemo from './components/context/context-demo1';
+// import ContextDemo from './components/context/context-demo1';
 // import MultipleContext from './components/multiple-context/context-demo';
-import MouseTracker from './components/render-props/render-demo'
+// import MouseTracker from './components/render-props/render-demo'
 // css put to bottom
 import './App.css';
 
@@ -20,6 +20,8 @@ const AsyncHeader = AsyncComponent(() => import('./components/Header'))
 const AsyncTodo = AsyncComponent(()=>import('./todo/index'))
 const AsyncEmail = AsyncComponent(() => import('./components/Email'))
 const AsyncClock = AsyncComponent(() => import('./components/Clock'));
+const AsyncEcharts = AsyncComponent(()=>import('./echarts'))
+
 class App extends Component {
 
 	// 组件更新前调用
@@ -56,8 +58,9 @@ class App extends Component {
 							search: '?email=me@johnny.com'
 						}} activeClassName="selected" replace>Email</NavLink>
 						<NavLink to="/todo" activeClassName="selected" replace>Todo list</NavLink>
-						<NavLink to="/context">Context Demo</NavLink>
-						<NavLink to = "/render_props" > Render Props Demo </NavLink>
+						{/* <NavLink to="/context">Context Demo</NavLink>
+						<NavLink to="/render_props" > Render Props Demo </NavLink> */}
+						<NavLink to = "/echarts" > Echarts</NavLink>
 					</div>
 					<WhiteSpace />
 					{/* router */}
@@ -65,8 +68,9 @@ class App extends Component {
 						<Route exact path="/" component={AsyncClock} />
 						<Route path="/email" component={AsyncEmail} />
 						<Route exact path="/todo" component={AsyncTodo} />
-						<Route path="/context" component={ContextDemo} />
-						<Route path="/render_props" component={MouseTracker}></Route>
+						{/* <Route path="/context" component={ContextDemo} />
+						<Route path="/render_props" component={MouseTracker}></Route> */}
+						<Route exact path="/echarts" component={AsyncEcharts} />
 					</Switch>
 				</div>
 			</Router>
